a155f3135b86b5c39946e301df491f0b11749a38
macintosh <macintosh@nyc-5.local>
2014-09-03 20:13:13 +0400

    jquery-fileupload-rails pipelined

--- lib/jquery/fileupload/rails/engine.rb
+++ lib/jquery/fileupload/rails/engine.rb
@@ -2,7 +2,7 @@ module JQuery
   module FileUpload
     module Rails
       class Engine < ::Rails::Engine
-        Rails.application.config.assets.precompile += %w( progressbar.gif loading.gif Jcrop.gif )
+        config.assets.precompile += %w( progressbar.gif loading.gif Jcrop.gif )
       end
     end
   end
